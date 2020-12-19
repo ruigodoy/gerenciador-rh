8cfcf0d5fe5aa62fdf5935d55222d9429b3ec593
ruigodoy <rui.godoy@hotmail.com>
2020-12-19 17:25:56 -0300

    Adicionado EAdição e um Colaborador

--- apps/colaboradores/urls.py
+++ apps/colaboradores/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from apps.colaboradores.views import ColaboradoresListar, ColaboradorEditar, ColaboradorDeletar
+from apps.colaboradores.views import (ColaboradoresListar,
+                                      ColaboradorEditar,
+                                      ColaboradorDeletar,
+                                      ColaboradorCriar
+                                      )
 
 urlpatterns = [
     path('', ColaboradoresListar.as_view(), name='listar_colaboradores'),
     path('editar/<int:pk>/', ColaboradorEditar.as_view(), name='atualizar_colaborador'),
     path('deletar/<int:pk>/', ColaboradorDeletar.as_view(), name='deletar_colaborador'),
+    path('novo/', ColaboradorCriar.as_view(), name='criar_colaborador'),
 ]
--- apps/colaboradores/views.py
+++ apps/colaboradores/views.py
@@ -1,5 +1,10 @@
+from django.contrib.auth.models import User
 from django.urls import reverse_lazy
-from django.views.generic import ListView, UpdateView, DeleteView
+from django.views.generic import (ListView,
+                                  UpdateView,
+                                  DeleteView,
+                                  CreateView
+                                  )
 
 from apps.colaboradores.models import Colaborador
 
@@ -20,3 +25,18 @@ class ColaboradorEditar(UpdateView):
 class ColaboradorDeletar(DeleteView):
     model = Colaborador
     success_url = reverse_lazy('listar_colaboradores')
+
+
+class ColaboradorCriar(CreateView):
+    model = Colaborador
+    fields = ['nome', 'departamentos']
+
+    def form_valid(self, form):
+        colaborador = form.save(commit=False)
+        username = colaborador.nome.split(' ')[0] + colaborador.nome.split(' ')[1]
+        colaborador.empresa = self.request.user.colaborador.empresa
+        colaborador.user = User.objects.create(username=username)
+        colaborador.save()
+
+        return super(ColaboradorCriar, self).form_valid(form)
+
