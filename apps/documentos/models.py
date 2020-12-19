2d5923ab36f3193f0e2ecce58573bede6ef0d1d5
ruigodoy <rui.godoy@hotmail.com>
2020-12-18 23:27:12 -0300

    Criando os relacionamentos do Banco de Dados

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from apps.colaboradores.models import Colaborador
 
 
 class Documento(models.Model):
     descricao = models.CharField(max_length=100, help_text='Descrição do Documento')
+    dono_documento = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.descricao
--- apps/registro_hora_extra/models.py
+++ apps/registro_hora_extra/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-
+from apps.colaboradores.models import Colaborador
 
 class RegistroHoraExtra(models.Model):
     motivo = models.CharField(max_length=100, help_text='Motivo da Hora Extra')
+    colaborador = models.ForeignKey(Colaborador, on_delete=models.PROTECT)
 
     def __str__(self):
         return self.motivo
