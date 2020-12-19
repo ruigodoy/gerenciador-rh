aef2cf17956cd06021ef7524fa0d25b884ba16e0
ruigodoy <rui.godoy@hotmail.com>
2020-12-19 09:29:52 -0300

    correções

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -3,9 +3,5 @@ from django.urls import path
 from apps.core.views import home
 
 urlpatterns = [
-<<<<<<< HEAD
     path('', home, name='home'),
-=======
-    path('', home),
->>>>>>> 9c9ea1061fa0b62b4203d85bdb7e0b97052cadd9
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
 from django.contrib.auth.decorators import login_required
 
 @login_required
-=======
-
->>>>>>> 9c9ea1061fa0b62b4203d85bdb7e0b97052cadd9
 def home(request):
     return render(request, 'core/index.html')
