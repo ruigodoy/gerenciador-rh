9c9ea1061fa0b62b4203d85bdb7e0b97052cadd9
ruigodoy <rui.godoy@hotmail.com>
2020-12-19 00:15:02 -0300

    Adicionado Core e Templates Basicos

--- gerenciador_rh/settings.py
+++ gerenciador_rh/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'gerenciador_rh.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates', 'apps/core/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -108,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
